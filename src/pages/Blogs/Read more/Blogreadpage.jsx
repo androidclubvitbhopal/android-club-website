6236437a4c018e85cdf05cfaa3a2937737d9b830
Mehul Singh <mehulsingh2022@vitbhopal.ac.in>
2024-08-01 23:06:19 +0530

    changes

--- src/pages/Blogs/Read more/Blogreadpage.jsx
+++ src/pages/Blogs/Read more/Blogreadpage.jsx
@@ -1,42 +1,46 @@
-import React,{useEffect} from "react";
+import React, { useEffect } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 
 const Blogreadpage = () => {
   const location = useLocation();
-  const history = useNavigate();
-  // eslint-disable-next-line no-unused-vars
-  const { title, date, tag, img, body } = location.state || {};
+  const navigate = useNavigate();
+  const { title,  img, body } = location.state || {};
 
   const handleBack = () => {
-    history.push("/blogs"); // Adjust the route according to your application's routing
+    navigate("/blogs"); 
   };
+
   const { pathname } = useLocation();
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
+
   return (
     <div className="bg-[#96BE96] pt-5 min-h-[100vh] w-full flex justify-center items-center">
-      <div className="rounded-xl bg-[#E6F1E8] min-h-[80vh] w-[95vw]">
-        <h3 className="p-[20px] text-[#254336] text-5xl font-semibold">{title}</h3>
-        <div className="flex justify-center">
+      <div className="rounded-xl bg-[#E6F1E8] min-h-[80vh] w-[95vw] p-6">
+        <h3 className="text-[#254336] text-5xl font-semibold mb-4">{title}</h3>
+        <div className="flex justify-center mb-6">
           <img
-            className="items-center rounded-lg h-[500px] w-[1275px] mb-4"
+            className="rounded-lg h-[500px] w-full object-cover"
             src={img}
             alt={title}
           />
         </div>
-        <div>
-          <p className="p-10 text-left text-black text-2xl whitespace-pre-wrap">{body}</p>
-          <div>
-            <Link to='/blogs'>
+        <div className="bg-white p-8 rounded-lg shadow-md">
+          <p className="text-black text-left text-xl whitespace-pre-wrap leading-relaxed">
+            {body}
+          </p>
+        </div>
+        <div className="flex justify-center mt-6">
+          <Link to='/blogs'>
             <button
-              className="flex flex-start ml-[35px] mb-5 p-5 text-l rounded-lg bg-[#96BE96]"
+              className="p-4 text-lg rounded-lg bg-[#254336] text-white hover:bg-[#2f4f4f] transition duration-300"
               onClick={handleBack}
             >
               Back to blogs
-            </button></Link>
-          </div>
+            </button>
+          </Link>
         </div>
       </div>
     </div>
